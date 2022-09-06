98f2ecac2d047e7b7cdf668b532ca4bbe8dbf7cf
Neil1977 <Neil_lunn1977@hotmail.com>
2022-09-06 11:59:19 +0100

    updated

--- home.js
+++ home.js
@@ -1,6 +1,21 @@
 
+const button = document.getElementById('see-review');
 
-showMessage();
+button.addEventListener('click', function()  {
+
+    const review = document.getElementById('review');
+
+    if (review.classList.contains('d-none')) {
+        review.classList.remove('d-none');
+        button.textContent = 'CLOSE REVIEW';
+    }
+    else {
+        review.classList.add('d-none');
+        button.textContent = 'SEE REVIEW';
+    }
+
+});
+
 
 
 
