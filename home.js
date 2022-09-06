8d7f6721a86eed243a3611f6e96ca89afdec9f77
Neil1977 <Neil_lunn1977@hotmail.com>
2022-09-06 12:52:40 +0100

    updated

--- home.js
+++ home.js
@@ -1,20 +1,11 @@
+const values = ['a', 'b', 'c' ];
 
-const button = document.getElementById('see-review');
-
-button.addEventListener('click', function()  {
-
-    const review = document.getElementById('review');
-
-    if (review.classList.contains('d-none')) {
-        review.classList.remove('d-none');
-        button.textContent = 'CLOSE REVIEW';
-    }
-    else {
-        review.classList.add('d-none');
-        button.textContent = 'SEE REVIEW';
-    }
-
+values.forEach(function(item) {
+    console.log(item);
 });
+
+
+
 
 
 
